169b116f82049b131aa984825a7a5a9f6198034d
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2019-10-25 02:08:35 +0700

    tmp commit

---
+++ common/models/bieu_nhap_lieu/bieu-nhap-lieu_chi-tieu.js
@@ -0,0 +1,10 @@
+let customCRUD = require('../../../utils/custom-crud')
+let app = require('../../../../server/server')
+
+'use_strict';
+
+module.exports = function (ThisModel) {
+  ThisModel.randomGenerateData = async function (uid, ma, ten, bieuNhapLieuId, chiTieuId, ghiChu) {
+    
+  }
+};
